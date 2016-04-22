abd2e5bc8ce3305a06a1d61b675a0f12df3ddba9
seonho <holys0210@snucse.org>
2016-04-22 16:23:39 +0900

    modify CTypeStringConst.GetType()

--- 2.Parsing/snuplc/src/ast.cpp
+++ 2.Parsing/snuplc/src/ast.cpp
@@ -1359,7 +1359,7 @@ bool CAstStringConstant::TypeCheck(CToken *t, string *msg) const
 
 const CType* CAstStringConstant::GetType(void) const
 {
-  return NULL;
+  return _type;
 }
 
 ostream& CAstStringConstant::print(ostream &out, int indent) const
